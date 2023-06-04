572d361ef256d07a00cab60ad1f9c1dca60c9589
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-06-04 15:26:13 +0200

    Added reading file
    Signed-off-by: Ewangelina <103572361+Ewangelina@users.noreply.github.com>

--- start.py
+++ start.py
@@ -5,24 +5,49 @@ import detect
 
 import os
 
-while True:
-	actions = input("Input the selected action profile numeral: \n")
+def from_file():
 	try:
+		f = open("values.txt")
+		actions = f.readline()
 		if int(actions) >= 0:
 			actions = int(actions)
-			break
+			if actions % 5 == 0:
+				email = input("Input e-mail address:\n")
+				mail.set_recipient(email)
+			
+			statistics.init_statistics()
+			action.set_option(actions)
+
+			print("Program starting, please wait")
+			detect.start()
 		else:
+			print("ERROR in values file")
+	except:
+		print("ERROR in values file")
+		
+def from_user():
+	while True:
+		try:
+			if int(actions) >= 0:
+				actions = int(actions)
+				break
+			else:
+				print("Incorrect action numeral inputed")
+		except:
 			print("Incorrect action numeral inputed")
-	except Exception as e:
-        	print("Incorrect action numeral inputed")
-
-if actions % 5 == 0:
-	email = input("Input e-mail address:\n")
-	mail.set_recipient(email)
-
 	
-statistics.init_statistics()
-action.set_option(actions)
+	if actions % 5 == 0:
+		email = input("Input e-mail address:\n")
+		mail.set_recipient(email)
 
-print("Program starting, please wait")
-detect.start()
+	statistics.init_statistics()
+	action.set_option(actions)
+
+	print("Program starting, please wait")
+	detect.start()
+
+actions = input("Input the selected action profile numeral: \n")
+if actions = "":
+	from_file()
+else:
+	from_user()
